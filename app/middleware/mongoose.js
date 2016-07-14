6072426cd390c9ac4c0d1b3b8c449c74adf02f90
esthercordova <estherpiacordova@gmail.com>
2016-07-14 14:22:22 -0400

    Add dbmongo lab link

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/hack-stash-test';
+const uri = process.env.MONGOLAB_URI || 'mongodb://SEMantic:mhshec3@ds015325.mlab.com:15325/hackstash';
+
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
